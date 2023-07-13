127ebbc54a5eb94d72d90fe0277ee5de16cf88dc
RAFSON JANI <aarjey1911@gmail.com>
2023-07-14 00:08:06 +0530

    Create Array_II_Find_Duplicate.cpp

---
+++ Array_II_Find_Duplicate.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+int findDuplicate(vector<int> &nums, int n){
+	// Write your code here.
+        int slow = nums[0] , fast = nums[nums[0]];
+        while(slow!=fast)
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        slow = 0;
+        while(slow!=fast)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        
+        return slow;	
+	
+}
